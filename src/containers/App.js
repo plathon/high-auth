6248d3811b551249d0fb9fa7a98246e2196da047
Renan Plathon <rplathon@gmail.com>
2016-02-01 23:54:48 -0200

    containers and reducers

---
+++ src/containers/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import ChatContainer from './ChatContainer'
+
+export default class App extends Component {
+  render() {
+    return (
+      <ChatContainer />
+    )
+  }
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from 'redux'
+import { default as chat } from './chat'
+
+export default combineReducers({ /**/ })
