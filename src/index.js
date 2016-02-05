bc3448a8dd6fc2887c976cfc099fc49de4c0384b
Renan Plathon <rplathon@gmail.com>
2016-02-05 01:34:37 -0200

    routing app

--- src/index.js
+++ src/index.js
@@ -1,22 +1,38 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
-import reducer from './reducers'
-import App from './containers/App'
+import * as reducers from './reducers'
+import { Router, Route, IndexRoute } from 'react-router'
+import createHistory from 'history/lib/createHashHistory'
+import { syncHistory, routeReducer } from 'react-router-redux'
+
+import Auth from './containers/AuthContainer'
+import Resgister from './containers/RegisterContainer'
+
+const history = createHistory()
+const synchronizedHistory = syncHistory(history)
+const reducer = combineReducers({
+  ...reducers,
+  routing: routeReducer
+})
 
 const middleware = process.env.NODE_ENV === 'production' ?
-  [ thunk ] :
-  [ thunk, logger() ]
+  [ thunk, synchronizedHistory ] :
+  [ thunk, logger(), synchronizedHistory ]
 
 const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore)
 const store = createStoreWithMiddleware(reducer)
 
 render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <Route path="/" component={Auth}/>
+      <Route path="signin" component={Auth}/>
+      <Route path="signup" component={Resgister}/>
+    </Router>
   </Provider>,
   document.getElementById('root')
 )
